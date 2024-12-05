56b611c306b6576c505a271c3bd8a4f43fd240bd
GoeSercan <sercan.goenuelcan@hotmail.com>
2024-12-05 18:29:23 +0100

    Initial commit

---
+++ src/main/java/com/carsharings/carsharingbillingservice/CarSharingBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carsharings.carsharingbillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingBillingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carsharings/carsharingbillingservice/CarSharingBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsharings.carsharingbillingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
